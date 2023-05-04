4e326398fce00ed4be1999b0df0d3491122bc021
jonathankerth <jonathanpkerth@gmail.com>
2023-05-04 15:42:43 -0700

    add resume png and styling

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -73,69 +73,11 @@ export default function Resume() {
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 			<Navbar />
-
 			<main className={styles.main}>
 				<h1 className={styles.title}>My Resume</h1>
-
-				<div className={styles.resumeContent}>
-					{/* Personal Information */}
-					<div className={styles.section}>
-						<h2 className={styles.sectionTitle}>Personal Information</h2>
-						<p>Name: Your Name</p>
-						<p>Email: your.email@example.com</p>
-						<p>Phone: (123) 456-7890</p>
-						<p>LinkedIn: linkedin.com/in/yourprofile</p>
-						<p>GitHub: github.com/yourusername</p>
-					</div>
-
-					{/* Skills */}
-					<div className={styles.section}>
-						<h2 className={styles.sectionTitle}>Skills</h2>
-						<p>JavaScript, React, Next.js, Node.js, Express, MongoDB, etc.</p>
-					</div>
-
-					{/* Experience */}
-					<div className={styles.section}>
-						<h2 className={styles.sectionTitle}>Experience</h2>
-						{/* Job 1 */}
-						<div>
-							<h3 className={styles.sectionSubtitle}>
-								Job Title - Company Name
-							</h3>
-							<p>Start Date - End Date</p>
-							<ul>
-								<li>Responsibility 1</li>
-								<li>Responsibility 2</li>
-								<li>Responsibility 3</li>
-							</ul>
-						</div>
-						{/* Additional jobs can be added here */}
-					</div>
-
-					{/* Education */}
-					<div className={styles.section}>
-						<h2 className={styles.sectionTitle}>Education</h2>
-						<div>
-							<h3 className={styles.sectionSubtitle}>
-								Degree - Institution Name
-							</h3>
-							<p>Start Year - End Year</p>
-						</div>
-						{/* Additional degrees can be added here */}
-					</div>
-
-					{/* Certifications */}
-					<div className={styles.section}>
-						<h2 className={styles.sectionTitle}>Certifications</h2>
-						<ul>
-							<li>Certification Name - Issuing Organization</li>
-							{/* Additional certifications can be added here */}
-						</ul>
-					</div>
-				</div>
-
+				<div className={styles.resumeImage}></div>
 				<div className={styles.download}>
-					<a href="" download>
+					<a href="/resume-image.png" download>
 						Download My Resume
 					</a>
 				</div>
