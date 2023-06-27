b0f8a8a3e41642790a0cba0b5b25e8f8451b7878
jonathankerth <jonathanpkerth@gmail.com>
2023-06-27 15:34:26 -0700

    fix mt issues

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -129,7 +129,7 @@ export default function Projects() {
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 			<Navbar />
-			<div className="flex flex-col items-center mt-30">
+			<div className="flex flex-col items-center mt-3">
 				<ul className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-1 gap-6 mt-15 ">
 					{projects.map((project) => (
 						<li
