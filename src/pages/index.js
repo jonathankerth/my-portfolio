eda2b1dbf01652b2f5584dfd1772b86b798b2050
jonathankerth <jonathanpkerth@gmail.com>
2023-07-16 17:29:02 -0700

    add black opacity background to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import Head from 'next/head'
 import Link from 'next/link'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div
+      className="flex flex-col items-center justify-center min-h-screen p-4 bg-center bg-cover"
+      style={{
+        backgroundImage:
+          "url('https://images.unsplash.com/photo-1533335121856-52d185e85ec1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80')",
+      }}
+    >
       <Head>
         <title>Software Engineer Portfolio</title>
         <meta
@@ -14,50 +19,60 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>Welcome,</h1>
+      <main className="flex flex-col items-center justify-center w-full px-8 py-8 text-center bg-black bg-opacity-60 max-w-2xl">
+        <h1 className="mb-4 text-3xl text-white">Welcome,</h1>
 
-        <p className={styles.description}>I&apos;m Jonathan Kerth</p>
+        <p className="mb-8 text-2xl text-white">I&apos;m Jonathan Kerth</p>
 
-        <div className={styles.grid}>
-          <Link href="/Projects" className={styles.card}>
+        <div className="grid w-full grid-cols-1 gap-4 mt-8 max-w-2xl">
+          <Link
+            href="/Projects"
+            className="p-6 text-left text-white border-4 border-white rounded-xl transition-all duration-300 ease-in-out bg-black bg-opacity-60 hover:border-black hover:bg-black hover:bg-opacity-80 focus:border-black focus:bg-black focus:bg-opacity-80 active:border-black active:bg-black active:bg-opacity-80"
+          >
             <h2>Projects &rarr;</h2>
             <p>Check out my projects!</p>
           </Link>
 
-          <Link href="/Resume" className={styles.card}>
+          <Link
+            href="/Resume"
+            className="p-6 text-left text-white border-4 border-white rounded-xl transition-all duration-300 ease-in-out bg-black bg-opacity-60 hover:border-black hover:bg-black hover:bg-opacity-80 focus:border-black focus:bg-black focus:bg-opacity-80 active:border-black active:bg-black active:bg-opacity-80"
+          >
             <h2>Resume &rarr;</h2>
             <p>View my resume</p>
           </Link>
 
-          <Link href="/About" className={styles.card}>
+          <Link
+            href="/About"
+            className="p-6 text-left text-white border-4 border-white rounded-xl transition-all duration-300 ease-in-out bg-black bg-opacity-60 hover:border-black hover:bg-black hover:bg-opacity-80 focus:border-black focus:bg-black focus:bg-opacity-80 active:border-black active:bg-black active:bg-opacity-80"
+          >
             <h2>About Me &rarr;</h2>
             <p>Learn more about my background and experience.</p>
           </Link>
 
-          <Link href="/Contact" className={styles.card}>
+          <Link
+            href="/Contact"
+            className="p-6 text-left text-white border-4 border-white rounded-xl transition-all duration-300 ease-in-out bg-black bg-opacity-60 hover:border-black hover:bg-black hover:bg-opacity-80 focus:border-black focus:bg-black focus:bg-opacity-80 active:border-black active:bg-black active:bg-opacity-80"
+          >
             <h2>Contact &rarr;</h2>
             <p>Get in touch with me for collaborations or opportunities.</p>
           </Link>
         </div>
       </main>
 
-      <footer className={styles.footer}>
+      <footer className="flex items-center justify-center w-full h-24 border-t border-white text-white">
         <a
           href="https://github.com/jonathankerth"
           target="_blank"
           rel="noopener noreferrer"
         >
           Powered by{' '}
-          <span className={styles.logo}>
-            <picture>
-              <img
-                src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
-                alt="GitHub Logo"
-                width={32}
-                height={32}
-              />
-            </picture>
+          <span className="h-4 ml-2">
+            <img
+              src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
+              alt="GitHub Logo"
+              width={32}
+              height={32}
+            />
           </span>
         </a>
       </footer>
