e965353f61ce0a026b9b71047d19c6f1536739de
jonathankerth <jonathanpkerth@gmail.com>
2023-07-25 20:57:04 -0700

    add nicCage API image

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -73,7 +73,7 @@ const projects = [
       'A fan page for Nicolas Cage movies built with React and Bootstrap. Hosted on Netlify. Built to display a backend API I built: NicCage API. ',
     link: 'https://github.com/jonathankerth/nicCage-client',
     link2: 'https://niccagecllient.netlify.app/',
-    image: '/nicCageHome.png',
+    image: '/images/nicCageHome.png',
   },
   {
     id: 2,
@@ -82,7 +82,7 @@ const projects = [
       'A serverless, PWA built with React/Node.js using TDD. It fetches upcoming tech events by utilizing the Google Calendar API and authentications using OAuth2. ',
     link: 'https://github.com/jonathankerth/meet',
     link2: 'https://jonathankerth.github.io/meet/',
-    image: '/meet-home.png',
+    image: '/images/meet-home.png',
   },
   {
     id: 3,
@@ -91,7 +91,7 @@ const projects = [
       'A Pokedex to display traits, pictures, and size of Pokemon. Utilizing the PokeAPI.co. Built with JavaScript.',
     link: 'https://github.com/jonathankerth/pokemon-js',
     link2: 'https://pokemon-js-omega.vercel.app/#',
-    image: '/pokedex-home.png',
+    image: '/images/pokedex-home.png',
   },
   {
     id: 4,
@@ -100,7 +100,7 @@ const projects = [
       'A GPT-3 powered kitchen assistant. Built with React, Node.js, Express.js. Backend hosted on Heroku and front-end deployed on Vercel. ',
     link: 'https://github.com/jonathankerth/KitchenGPT',
     link2: 'https://kitchen-gpt.vercel.app/',
-    image: '/kitchen-gpt-home.png',
+    image: '/images/kitchen-gpt-home.png',
   },
   {
     id: 5,
@@ -109,7 +109,7 @@ const projects = [
       'Built on Next.js using Tailwind CSS to display real-time weather data from the OpenWeather API. Deployed here using Vercel: https://weather-app-woad-two.vercel.app/',
     link: 'https://github.com/jonathankerth/weather-app',
     link2: 'https://weather-app-woad-two.vercel.app/',
-    image: '/weather-home.png',
+    image: '/images/weather-home.png',
   },
   {
     id: 6,
@@ -118,8 +118,7 @@ const projects = [
       'A RESTful API built with Node.js, Express.js, MongoDB, and hosted on Heroku.',
     link: 'https://github.com/jonathankerth/Nicolas-Cage-API',
     link2: 'https://niccage.herokuapp.com/',
-    image:
-      'https://storagebucket-730.s3.us-west-2.amazonaws.com/Screen%20Shot%202023-07-25%20at%208.39.41%20PM.png?response-content-disposition=inline&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEKT%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEaCXVzLXdlc3QtMiJHMEUCIDZ0%2FLSpHoimn5M2XsXfCG6Tj7qpCkHW4dJNlGPKiHzSAiEAy14xmdGHdo7rKQ4wqsgrD7Dwgh25prceZtkLX7Melx4q6AIIPRAAGgw1Nzc5Njk1OTIzOTIiDECjQJkpi6AIIbKIRCrFAr2X45zJMn4CdieBIZZC2ibSVcQ5kpU%2BuRcEQkLotPq3oG%2F%2FHt%2BES9fSchIM%2FtvOq0FQoNuq%2FU%2F7l5SrLY134zV2QEolPBUHyNkN3D5cvWmRaAwoh4zwPn%2BRddX9vi6cSCxiP2bDWUs0kJ49RBFoDX34LkaQnvQeQjln7JpNZBsFDjnXkfR9xj0r4yfkVcZLheDH2rE7eKTq8nhJ5PE1nbY4TIscBbYmCAsyJfGtEEFnK%2FgUwY%2BpW9ILdPnaMlvmh7jxpQbZbcfFvw56eH%2BKHMdiPvpOegixqQpnY5tS6JE5EIjTjtzJCosG6%2FuFoyoKl%2BLRYxwdaBN1UUEaGIHt6pkkyjSiEmNvCbWluRK7Kp1%2F5IEUDSjwsQBe62PhK%2Byh5APk7WOhPFcZ7Xw%2BBTTwHUFN2ozHDIYFrRFVdFr%2FdZK04l7Zz3Aww6uCpgY6swJtPwR%2BOP8a46V6iRvFceK6LJVWhETiRX8%2BQ64d%2FBTggUYf6DH1uZZJAzS%2F5cVE2Kfm%2FUEPOt4WUJSeGxJfDks4uqw0%2Bi%2FPTnAVZv%2FJMInQ8rcuvfEZuAckfQmZtBWaXJ0DGLEJxPOX0CAb4xqJ1iIqL8qLnrqRadew%2BpbpSrv1oXy%2Bbd3fZhUl51Xf5zXOUNM1fBWiA2qXQ7u%2FpGitTyIuUWG4mHJjS3TIvGtJMCT%2F%2FIR7E00Ik9UC4%2Fbb24JR%2F4MjzAQ367Bj7gt9%2FuIRDhWaGBTCGgDfL8Ki%2FSf3V2zJ%2F59OonlnXdxTtLnuBqnp9chKx1ESOwh6zm30wDgM7YjvSnuf9OKY28Lv3uGWvGRnEahhILDclMrlv54GVEvaPQi6nQytlsvgHZvRCRsj5cf%2BdP8j&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20230726T034137Z&X-Amz-SignedHeaders=host&X-Amz-Expires=300&X-Amz-Credential=ASIAYNENMDBEOM7SRHPR%2F20230726%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Signature=8cc1168b1be4dbf159aee64e3253a29da366af1aeaf3c19857522303558140aa',
+    image: '/images/nicCage-API.png',
   },
   {
     id: 7,
