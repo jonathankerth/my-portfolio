0f5010c570b8e80fd9eab22ca255da840e874a40
jonathankerth <jonathanpkerth@gmail.com>
2023-08-05 15:48:40 -0700

    change modal button to close

--- src/components/ProjectModal.js
+++ src/components/ProjectModal.js
@@ -30,7 +30,7 @@ const ProjectModal = ({ project, closeModal }) => {
                 className="px-4 py-2 ml-4 bg-red-500 text-white rounded hover:bg-red-600"
                 onClick={closeModal}
               >
-                Cancel
+                Close
               </button>
             </div>
           </div>
