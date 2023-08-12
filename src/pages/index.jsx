23e0a02995ed5d017e60767d0076c9a259f29c87
jonathankerth <jonathanpkerth@gmail.com>
2023-08-12 00:59:19 -0700

    make footer small

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -178,16 +178,7 @@ export default function Home() {
         </div>
       </main>
 
-      <footer className="fixed left-0 bottom-0 w-full bg-gray-900/50 text-white">
-        <div className="flex items-center justify-center h-24">
-          <a
-            href="https://github.com/jonathankerth"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="flex items-center space-x-2 text-white hover:text-gray-200"
-          ></a>
-        </div>
-      </footer>
+      <footer className="fixed left-0 bottom-0 w-full h-6 bg-gray-900 text-white z-50"></footer>
     </div>
   )
 }
