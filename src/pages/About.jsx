29ad20e95f8d8470dad22487aac1fc0cd2d7ae43
jonathankerth <jonathanpkerth@gmail.com>
2023-08-21 13:20:10 -0700

    reduce font size of main

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -178,38 +178,39 @@ export default function About() {
       <Navbar />
 
       <main className="flex flex-col items-center justify-center flex-1 py-12 mt-10">
-        <div className="bg-black bg-opacity-40 rounded-lg p-2 mb-6 max-w-3xl">
-          <p className="text-white text-2xl leading-7 text-center max-w-full mb-6">
+        <div className="bg-black bg-opacity-40 rounded-lg p-4 mb-6 max-w-2xl">
+          <p className="text-white text-md leading-6 text-center max-w-full mb-4">
             I&apos;m a dedicated and passionate software engineer specializing
             in building scalable, efficient web applications. With expertise in
             cutting-edge technologies like React, Next.js, Node.js, Express,
             ChatGPT, and MongoDB, I&apos;m always ready to tackle new challenges
             and learn in the process.
           </p>
-          <p className="text-white text-2xl leading-7 text-center max-w-full mb-2">
+          <p className="text-white text-md leading-6 text-center max-w-full">
             Outside the world of code, I&apos;m a food enthusiast, self
             proclaimed nerd, home cook, pet parent, and traveling the globe with
             my partner. Feel free to connect for any opportunities or questions
             about my projects. Thanks for dropping by!
           </p>
         </div>
-        <h3 className="bg-black bg-opacity-40 text-white  text-center text-2xl p-6 rounded-lg ">
+        <h3 className="bg-black bg-opacity-40 text-white text-center text-xl p-4 rounded-lg">
           A few technologies I&apos;ve been working with recently:
         </h3>
-        <div className="flex flex-wrap justify-center items-center mt-6">
+        <div className="flex flex-wrap justify-center items-center mt-4">
           {techStack.map((tech, index) => (
             <a
               key={index}
               href={tech.url}
               target="_blank"
               rel="noopener noreferrer"
-              className="inline-block bg-red-200 rounded-full px-3 py-1 text-lg font-semibold text-gray-800 m-2 shadow-md hover:bg-red-500 transition-colors duration-300"
+              className="inline-block bg-red-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-800 m-1 shadow-md hover:bg-red-500 transition-colors duration-300"
             >
               {tech.name}
             </a>
           ))}
         </div>
       </main>
+
       <div className="max-w-xl">
         <ImageCarousel />
       </div>
