f21a1580de32432a881738163b4e1c53010bc02a
jonathankerth <jonathanpkerth@gmail.com>
2023-08-22 23:40:05 -0700

    add only img upload rule

--- src/pages/api/s3Url.js
+++ src/pages/api/s3Url.js
@@ -2,12 +2,12 @@ import { generateUploadURL } from '../../../lib/s3'
 
 export default async (req, res) => {
   if (req.method === 'GET') {
-    const fileType = req.query.fileType
     try {
+      const fileType = req.query.fileType
       const url = await generateUploadURL(fileType)
       res.status(200).send({ url })
     } catch (error) {
-      res.status(400).send(error.message)
+      res.status(400).send({ error: error.message })
     }
   } else {
     res.status(405).end() // Method Not Allowed
