fb3da937df00c27f68cd197cbe215e3cec8cbfa3
jonathankerth <jonathanpkerth@gmail.com>
2023-10-30 22:14:33 -0700

    add light/dark mode

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useCallback } from 'react'
 import Link from 'next/link'
-import { useRouter } from 'next/router'
-import { FaBars, FaTimes } from 'react-icons/fa'
+import { FaBars, FaTimes, FaSun, FaMoon } from 'react-icons/fa'
+import { useTheme } from 'next-themes'
 
 const navLinks = [
   { href: '/', label: 'Home' },
@@ -13,21 +13,15 @@ const navLinks = [
 
 const Navbar = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false)
-  const [prevScrollPos, setPrevScrollPos] = useState(0)
-  const [visible, setVisible] = useState(true)
-  const router = useRouter()
+  const { theme, setTheme } = useTheme()
 
   const toggleMobileMenu = () => {
     setIsMobileMenuOpen(!isMobileMenuOpen)
   }
 
   const handleScroll = useCallback(() => {
-    const currentScrollPos = window.pageYOffset
-    const isVisible = prevScrollPos > currentScrollPos
-
-    setPrevScrollPos(currentScrollPos)
-    setVisible(isVisible)
-  }, [prevScrollPos])
+    // Add your custom scroll handling logic here if needed
+  }, [])
 
   useEffect(() => {
     window.addEventListener('scroll', handleScroll)
@@ -39,55 +33,76 @@ const Navbar = () => {
 
   return (
     <nav
-      className={`fixed top-0 left-0 z-50 w-full py-4 bg-gradient-to-b from-gray-800 to-black transition-all duration-200 ${
-        visible ? 'visible' : 'invisible'
+      className={`fixed top-0 left-0 z-50 w-full py-4 transition-all duration-200 ${
+        isMobileMenuOpen || theme === 'dark' ? 'bg-black' : 'bg-white'
       }`}
     >
       <div className="max-w-screen-xl mx-auto px-4 md:px-8 flex justify-between items-center">
+        <button
+          onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+          type="button"
+          className={
+            theme === 'dark'
+              ? 'text-white hover:text-gray-300'
+              : 'text-black hover:text-gray-600'
+          }
+          aria-label="toggle theme"
+        >
+          {theme === 'dark' ? <FaSun /> : <FaMoon />}
+        </button>
+
         <div className="hidden md:block">
           <ul className="flex space-x-6">
-            {navLinks.map(
-              ({ href, label }) =>
-                router.pathname !== href && (
-                  <li key={href}>
-                    <Link
-                      href={href}
-                      className="px-4 py-2 font-medium text-white hover:text-gray-200 transition duration-300"
-                    >
-                      {label}
-                    </Link>
-                  </li>
-                )
-            )}
+            {navLinks.map(({ href, label }) => (
+              <li key={href}>
+                <Link
+                  href={href}
+                  className={`px-4 py-2 font-medium transition duration-300 ${
+                    isMobileMenuOpen || theme === 'dark'
+                      ? 'text-white hover:text-gray-300'
+                      : 'text-black hover:text-gray-600'
+                  }`}
+                >
+                  {label}
+                </Link>
+              </li>
+            ))}
           </ul>
         </div>
+
         <div className="md:hidden flex items-center">
           <button
             onClick={toggleMobileMenu}
             type="button"
-            className="text-white hover:text-gray-200 focus:outline-none focus:text-gray-200"
+            className={
+              theme === 'dark'
+                ? 'text-white hover:text-gray-300'
+                : 'text-black hover:text-gray-600'
+            }
             aria-label="toggle menu"
           >
             {isMobileMenuOpen ? <FaTimes /> : <FaBars />}
           </button>
         </div>
       </div>
+
       {isMobileMenuOpen && (
         <div className="md:hidden absolute top-12 right-0 w-full bg-gray-900/90">
           <ul className="px-8 py-4">
-            {navLinks.map(
-              ({ href, label }) =>
-                router.pathname !== href && (
-                  <li key={href} className="py-2">
-                    <Link
-                      href={href}
-                      className="text-white hover:text-gray-200 text-lg font-semibold"
-                    >
-                      {label}
-                    </Link>
-                  </li>
-                )
-            )}
+            {navLinks.map(({ href, label }) => (
+              <li key={href} className="py-2">
+                <Link
+                  href={href}
+                  className={`text-lg font-semibold ${
+                    theme === 'dark'
+                      ? 'text-white hover:text-gray-300'
+                      : 'text-black hover:text-gray-600'
+                  }`}
+                >
+                  {label}
+                </Link>
+              </li>
+            ))}
           </ul>
         </div>
       )}
