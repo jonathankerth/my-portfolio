58a33dc4a8af283f67c44eacffddabd79c17da45
jonathankerth <jonathanpkerth@gmail.com>
2024-02-05 10:19:23 -0800

    set default theme to light

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,12 +2,11 @@ import '../../globals.css'
 import { Analytics } from '@vercel/analytics/react'
 import { ThemeProvider } from 'next-themes'
 
-function MyApp({ Component, pageProps }) {
+export default function App({ Component, pageProps }) {
   return (
-    <ThemeProvider attribute="class">
+    <ThemeProvider attribute="class" enableSystem={true} defaultTheme="light">
       <Component {...pageProps} />
+      <Analytics />
     </ThemeProvider>
   )
 }
-
-export default MyApp
