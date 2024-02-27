81e2a0451a1714ed645b2488f481273ceb2db535
jonathankerth <jonathanpkerth@gmail.com>
2024-02-27 14:00:16 -0600

    remove unused technologies

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -177,7 +177,7 @@ export default function Projects() {
             theme === 'dark' ? 'text-[#ECF0F1]' : 'text-[#154360]'
           }`}
         >
-          A Few of the Projects I've Built
+          A Few of My Projects
         </h1>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 px-4">
           {projects.map((project) => (
