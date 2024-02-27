099cbbc31e926a7a8b240abb32b802beb266b291
jonathankerth <jonathanpkerth@gmail.com>
2024-02-27 14:00:06 -0600

    improve section description

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -24,7 +24,6 @@ export default function About() {
     { name: 'Bootstrap', url: 'https://getbootstrap.com/' },
     { name: 'Tailwind CSS', url: 'https://tailwindcss.com/' },
     { name: 'Redux', url: 'https://redux.js.org/' },
-    { name: 'Atatus', url: 'https://www.atatus.com/' },
     { name: 'Jest', url: 'https://jestjs.io/' },
     { name: 'Cucumber', url: 'https://cucumber.io/' },
     {
@@ -33,24 +32,20 @@ export default function About() {
     },
     { name: 'HTML', url: 'https://developer.mozilla.org/en-US/docs/Web/HTML' },
     { name: 'CSS', url: 'https://developer.mozilla.org/en-US/docs/Web/CSS' },
+    { name: 'SCSS', url: 'https://sass-lang.com/' },
     {
       name: 'JavaScript',
       url: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript',
     },
     { name: 'TypeScript', url: 'https://www.typescriptlang.org/' },
-    { name: 'SCSS', url: 'https://sass-lang.com/' },
+
     {
       name: 'Google Analytics',
       url: 'https://developers.google.com/analytics',
     },
     { name: 'Firebase', url: 'https://firebase.google.com/' },
     { name: 'Python', url: 'https://www.python.org/' },
-    { name: 'Flask', url: 'https://flask.palletsprojects.com/' },
-    {
-      name: 'Beautiful Soup',
-      url: 'https://www.crummy.com/software/BeautifulSoup/',
-    },
-    { name: 'Requests', url: 'https://docs.python-requests.org/en/latest/' },
+
     { name: 'JSON', url: 'https://www.json.org/json-en.html' },
     { name: 'Heroku', url: 'https://www.heroku.com/' },
   ]
@@ -67,7 +62,7 @@ export default function About() {
         <title>About Me</title>
         <meta
           name="description"
-          content="Discover more about my experience and the diverse technologies I utilize"
+          content="Discover more about my experience and the technologies I utilize"
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
@@ -92,21 +87,27 @@ export default function About() {
               theme === 'dark' ? 'text-[#ECF0F1]' : 'text-[#154360]'
             }`}
           >
-            I'm a dedicated and passionate software engineer specializing in
-            building scalable, and efficient web applications. I love tackling
-            new challenges by using the best technology for the project. I also
-            belive in the importance of continuous learning in all aspects of
-            life but especially software developement.
+            I'm a passionate software engineer from Portland, Oregon, USA. I
+            specialize in building web applications primarily with Javascript
+            and Python technologies. I love building software that makes a
+            positive impact in the world and I'm always trying to learn new
+            things.
           </p>
           <p
-            className={`text-xl leading-6 text-center ${
+            className={`text-xl leading-6 text-center mb-4 ${
               theme === 'dark' ? 'text-[#ECF0F1]' : 'text-[#154360]'
             }`}
           >
             Outside the world of code, I'm a food enthusiast, self-proclaimed
-            nerd, home chef, pet parent, and I've been to 5 out of the 7
-            continents. Feel free to connect for any opportunities or questions
-            about my projects. Thanks for dropping by!
+            nerd, home cook, pet parent, 3D printer hobbyist and I've been to 5
+            out of the 7 continents.
+          </p>
+          <p
+            className={`text-xl leading-6 text-center mb-4 ${
+              theme === 'dark' ? 'text-[#ECF0F1]' : 'text-[#154360]'
+            }`}
+          >
+            Thanks for dropping by my portofolio website!
           </p>
         </div>
         <TechStack techStack={techStack} theme={theme} />
